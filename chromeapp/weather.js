ce0f713ca56c33cfac0583f5b63e29833208576c
한지현 <gkswlgus923@gamil.com>
2021-03-23 23:53:30 +0900

    weather 기능 추가 진행 중

---
+++ chromeapp/weather.js
@@ -0,0 +1,27 @@
+
+const COORDS = 'coords';
+
+function handleGeoSucces(position){
+    console.log(position);
+}
+function handleGeoError(){
+    console.log("Cant access geo location");
+}
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces,handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCords = localStorage.getItem(COORDS);
+    if(loadedCords === null){
+        askForCoords();
+    }else{
+        //getWeather
+    }
+}
+
+function init(){
+
+}
+
+init();
